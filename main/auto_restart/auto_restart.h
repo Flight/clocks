2558a1638bdb8780d8b0bbb9926a70af60b7810a
Flight <flight.neg@gmail.com>
2023-11-19 19:05:12 +0100

    Auto restart by timeout removed

--- main/auto_restart/auto_restart.h
+++
@@ -1,1 +0,0 @@
-void auto_restart_task(void *pvParameter);
--- main/main.c
+++ main/main.c
@@ -21,7 +21,6 @@
 #include "temperature_from_sensor/temperature_from_sensor.h"
 #include "weather_from_api/weather_from_api.h"
 #include "ota_update/ota_update.h"
-#include "auto_restart/auto_restart.h"
 
 EventGroupHandle_t global_event_group;
 bool global_is_light_on = false;
@@ -55,6 +54,4 @@ void app_main(void)
   xTaskCreatePinnedToCore(&weather_from_api_task, "Weather from API", configMINIMAL_STACK_SIZE * 3, NULL, 1, NULL, 1);
   xTaskCreatePinnedToCore(&ota_update_task, "OTA Update", configMINIMAL_STACK_SIZE * 4, NULL, 1, NULL, 1);
   xTaskCreatePinnedToCore(&system_state_task, "System State", configMINIMAL_STACK_SIZE * 2, NULL, 1, NULL, 1);
-
-  // xTaskCreatePinnedToCore(&auto_restart_task, "Auto Restart Task", configMINIMAL_STACK_SIZE * 2, NULL, 1, NULL, 1);
 }
